f5ee862a0e292eb14f91c89e64024d7119c9ef64
kh <magdyah319@gmail.com>
2024-08-07 04:16:18 +0300

    adding animation to splash_view

--- lib/features/presentation/view/widgets/splashViewBody.dart
+++ lib/features/presentation/view/widgets/splashViewBody.dart
@@ -1,10 +1,43 @@
 import 'package:bookley_appp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({
     super.key,
   });
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<Offset> slidingAnimationLogo;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: const Offset(0, 0))
+            .animate(animationController);
+    slidingAnimationLogo =
+        Tween<Offset>(begin: const Offset(-10, 0), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(
+      () => setState(() {}),
+    );
+
+  }
+  @override
+  void dispose() {
+
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +45,19 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
+        SlideTransition(
+            position: slidingAnimationLogo,
+            child: Image.asset(AssetsData.logo)),
         const SizedBox(
           height: 4,
         ),
-        const Text('Read To Understand',
-
-        textAlign: TextAlign.center,),
+        SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read To Understand',
+            textAlign: TextAlign.center,
+          ),
+        ),
       ],
     );
   }
